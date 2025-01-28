5b94e23f627bdbf0fa7f0222ae8a7fd65268bf29
Jeann Porfírio <jeannporfirio@gmail.com>
2025-01-28 17:27:05 -0300

    adding form validation (ch14)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,20 +7,42 @@ import { z } from "zod";
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce.number().gt(0, "Please enter an amount greater than $0"),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status",
+  }),
   date: z.string(),
 });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
-  const data = CreateInvoice.parse({
+export async function createInvoice(previousState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-  const amountInCents = data.amount * 100;
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing fields. Failed to create invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
   const client = await db.connect();
@@ -28,10 +50,10 @@ export async function createInvoice(formData: FormData) {
   try {
     await client.sql`
     INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${data.customerId}, ${amountInCents}, ${data.status}, ${date})
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (err) {
-    console.warn(err);
+    return { message: "Database Error: Failed to create invoice" };
   } finally {
     client.release();
   }
